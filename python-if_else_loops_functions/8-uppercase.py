7a9b63fb3528f99dccd9e8cfd4e5f0a986f2982d
jeremias <jeremiaserba7894@gmail.com>
2023-05-29 18:23:27 +0000

    Task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    for element in str:
+    for element in range(len(str)):
         elementN = ord(str[element])
         if (elementN >= 97 and elementN <= 122):
             elementN = elementN - 32
