a2e21b54bf433d004fbe4578586cb3488d257785
jeremias <jeremiaserba7894@gmail.com>
2023-05-29 18:54:06 +0000

    Task 12 - Fix by pycodestyle

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    for element in range(0, 101):
+    for element in range(1, 101):
         if element % 3 == 0 and element % 5 == 0:
             print("FizzBuzz", end=" ")
         elif element % 3 == 0:
