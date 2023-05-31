6a93d964cfb27f6507a00941385c283655fcef7f
jeremias <jeremiaserba7894@gmail.com>
2023-05-31 01:15:06 +0000

    Project 2 - Task 6 - Fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for first in range(9):
+for first in range(8):
     for second in range(1, 10):
         if first < second:
             print("{}{}".format(first, second), end=", ")
-print(98)
+print(89)
