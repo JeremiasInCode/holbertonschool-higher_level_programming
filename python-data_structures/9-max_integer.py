b63dd672ee8ad4cf6049ce4367656becd6bd3530
jeremias <jeremiaserba7894@gmail.com>
2023-06-05 18:22:43 +0000

    Task 9 - Assing of value after of verification

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_value = my_list[0]
 
     if len(my_list) == 0 or my_list == []:
         return None
+
+    max_value = my_list[0]
 
     for element in my_list:
         if element > max_value:
