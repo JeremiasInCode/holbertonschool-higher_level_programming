edc097f41aa8074cfa1f354f0a9c3cb5016b41a1
jeremias <jeremiaserba7894@gmail.com>
2023-06-05 15:50:57 +0000

    Task 4 - Add permissions and fix by pycodestyle

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ def new_in_list(my_list, idx, element):
     lenghtArr = len(my_list)
     if not my_list or idx < 0 or idx >= lenghtArr:
         return my_list
-    
+
     new_list = my_list.copy()
     new_list[idx] = element
     return new_list
