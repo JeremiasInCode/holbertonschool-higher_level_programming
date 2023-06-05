4ea4171b39f1c9bb176b9cacaf2da7facccfac15
jeremias <jeremiaserba7894@gmail.com>
2023-06-05 21:25:53 +0000

    Task 7 - Delete loop

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,9 +2,9 @@
 def update_dictionary(a_dictionary, key, value):
     if (a_dictionary == []):
         return
-    for element in a_dictionary:
-        if element in a_dictionary:
-            a_dictionary.update({element: value})
-        else:
-            a_dictionary[element] = value
+
+    if key in a_dictionary:
+        a_dictionary.update({key: value})
+    else:
+        a_dictionary[key] = (value)
     return (a_dictionary)
