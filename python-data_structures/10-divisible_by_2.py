6c872d279824d2a1b5876efe6dd97fbb84995301
jeremias <jeremiaserba7894@gmail.com>
2023-06-05 18:46:38 +0000

    Task 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    multiply_of_two = my_list.clear()
+    if len(my_list) == 0 or my_list == []:
+        return []
 
-    if (len(my_list) == 0 or my_list == []):
-        return None
-
-    add_true = multiply_of_two.append(True)
-    add_false = multiply_of_two.append(False)
-    
-    for element in my_list:
-        add_true if element % 2 == 0 else add_false
-
-    return (multiply_of_two)
+    # comprehension checklist - iterates on element.
+    multiply = [True if element % 2 == 0 else False for element in my_list]
+    return multiply
