1e363f2a0e91ea9afd20e3aa4d296dfacc7c1cea
jeremias <jeremiaserba7894@gmail.com>
2023-06-05 18:20:14 +0000

    Task 9 - Change of initial value of variable

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_value = 0
+    max_value = my_list[0]
 
     if len(my_list) == 0 or my_list == []:
         return None
 
     for element in my_list:
-        if (element > max_value):
+        if element > max_value:
             max_value = element
 
     return max_value
