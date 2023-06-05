a7187838c0bbe3d9c0a2979292a1d81099c70323
jeremias <jeremiaserba7894@gmail.com>
2023-06-05 16:20:26 +0000

    Task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    separator = " "
+
     if not matrix:
         return
 
-    separator = " "
     for row in matrix:
         print(separator.join("{:d}".format(element) for element in row))
