60d1d51807b1d184c0150f3da6515c906c9b2433
jeremias <jeremiaserba7894@gmail.com>
2023-06-05 21:51:33 +0000

    Task 9 - Add function for to have key and value using items()

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_list = a_dictionary.copy()
-    for index in a_dictionary:
-        new_list[index] = new_list[index] * 2 
+    for index, element in new_list.items():
+        multiply = element * 2
+        new_list[index] = multiply
     return new_list
