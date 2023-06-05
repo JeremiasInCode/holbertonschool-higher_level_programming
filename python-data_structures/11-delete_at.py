f125b61a752a4bb9e4cc459ea17ee5629140881a
jeremias <jeremiaserba7894@gmail.com>
2023-06-05 18:52:45 +0000

    Task 11

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if my_list == [] or idx < 0 or idx >= len(my_list):
+        return my_list
+
+    for element in my_list:
+        if element == idx:
+            del my_list[element]
+    return (my_list)
