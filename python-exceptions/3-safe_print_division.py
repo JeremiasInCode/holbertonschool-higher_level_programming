03f81a09991b3d0bcffc06758fade2da5b0d6794
jeremias <jeremiaserba7894@gmail.com>
2023-06-06 18:17:08 +0000

    Task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,11 +2,22 @@
 def safe_print_division(a, b):
     try:
         result = a // b
-    except ZeroDivisionError:
-        result = None
     except Exception:
-        print("A exception occurred")
         result = None
     finally:
         print("Inside result: {}".format(float(result) if result else result))
     return float(result) if result is not None else result
+
+
+def main():
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+main()
