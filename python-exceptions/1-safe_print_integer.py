2bf1c99ef63ac248ce3325b3caaa3692043f07ab
jeremias <jeremiaserba7894@gmail.com>
2023-06-06 17:02:29 +0000

    Task 1 - Add more general exception and delete unnecessary condition

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if not value:
-        return
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as err:
+    except:
         return False
