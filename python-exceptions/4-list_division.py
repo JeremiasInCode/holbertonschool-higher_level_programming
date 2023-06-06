dd32a5bb795ac336dc6ce80ed1c4cbd2558a8b24
jeremias <jeremiaserba7894@gmail.com>
2023-06-06 18:45:53 +0000

    Task 4 - Add finally

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    list = [0] * list_length
+    list = []
     for element in range(list_length):
+        list.append(0)
         try:
             list[element] = my_list_1[element] / my_list_2[element]
         except TypeError:
@@ -10,4 +11,6 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
         except IndexError:
             print("out of range")
+        finally:
+            pass
     return (list)
