c6b79d4198500a9e64b3131e8b4b0bfcf742e52f
jeremias <jeremiaserba7894@gmail.com>
2023-06-06 18:21:50 +0000

    Task 3 - Add message for None case

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,8 +3,7 @@ def safe_print_division(a, b):
     try:
         result = a // b
     except Exception:
-        print("Inside result: {}".format(None))
         result = None
     finally:
         print("Inside result: {}".format(float(result) if result else result))
-    return float(result) if result is not None else result
+    return float(result) if result else result
