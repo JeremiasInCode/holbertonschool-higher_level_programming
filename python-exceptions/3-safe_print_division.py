131bbf9eabdee5c210505d16499073d92316ffd1
jeremias <jeremiaserba7894@gmail.com>
2023-06-06 18:17:59 +0000

    Task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -7,17 +7,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(float(result) if result else result))
     return float(result) if result is not None else result
-
-
-def main():
-    a = 12
-    b = 2
-    result = safe_print_division(a, b)
-    print("{:d} / {:d} = {}".format(a, b, result))
-
-    a = 12
-    b = 0
-    result = safe_print_division(a, b)
-    print("{:d} / {:d} = {}".format(a, b, result))
-
-main()
