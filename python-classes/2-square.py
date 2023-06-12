6f5a9b01d437507149624d2ee1c18289d109e4a3
jeremias <jeremiaserba7894@gmail.com>
2023-06-12 18:54:14 +0000

    Task 2 - Change TypeError to ValueError

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         if (type(size) == int):
             if (size < 0):
-                TypeError("size must be >= 0")
+                ValueError("size must be >= 0")
             else:
                 self.__size = size
         else:
