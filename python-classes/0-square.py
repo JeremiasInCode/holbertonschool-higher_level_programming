c136dc39df526871a83f1ce173e4d416dfd0f34b
jeremias <jeremiaserba7894@gmail.com>
2023-06-12 12:53:15 +0000

    Task 0 - Changes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon June 12 9:50:00 2023.
-
-@author: JeremiasInCode
-@description: Creation of a class called Square
-
-"""
+""" Task 0 """
 
 
 class Square:
-    """Empty class that defines a square"""
+    """Empty class that defines a square."""
     pass
