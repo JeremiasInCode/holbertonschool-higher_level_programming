f2ec27118634448ccd840c87d23b80b7395ac52b
jeremias <jeremiaserba7894@gmail.com>
2023-06-13 18:22:00 +0000

    Task 4 - Removed additional function

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Task 4"""
 
+
 def print_square(size):
     error = "size must be an integer"
 
     if not isinstance(size, int):
         raise TypeError(error)
-    elif type(size) == float and size < 0:
-        raise TypeError(error)
-    elif size < 0:
+
+    if size < 0:
         raise ValueError("size must be >= 0")
 
     for element1 in range(size):
