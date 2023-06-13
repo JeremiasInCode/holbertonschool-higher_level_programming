ed74aabf989daabe714d93c8bc5b54f0d5bb4027
jeremias <jeremiaserba7894@gmail.com>
2023-06-13 17:41:29 +0000

    Task 2 - Fix erros by pycodestyle and add new test

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
 """Task 2"""
 
-def matrix_divided(matrix, div):
-    """ Divide a matrix. """
 
-    general_error = "matrix must be a matrix (list of lists) of integers/floats"
+def matrix_divided(matrix, div):
+    """Divide a matrix."""
+    error = "matrix must be a matrix (list of lists) of integers/floats"
     size_error = "Each row of the matrix must have the same size"
 
-    """ 
-    Verify that the general matrix is not a list and each (all) 
-    row is not of type list in the matrix. In this case, show the
-    error saved in a variable.
-    """
     if (not isinstance(matrix, list) or
-        not all(isinstance(element, list) for element in matrix)):
-            raise TypeError(general_error)
+            not all(isinstance(element, list) for element in matrix)):
+        raise TypeError(error)
+
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     new_matrix = []
     buffer_row = []
     len_original_row = len(matrix[0])
@@ -28,11 +25,11 @@ def matrix_divided(matrix, div):
         if len_original_row != len(row):
             raise TypeError(size_error)
         for element in row:
-            if isinstance(element, (int, float)):
+            if element % div == 0 and isinstance(element, (int, float)):
                 buffer_row.append(round(element / div, 2))
             else:
-                raise TypeError(general_error)
+                raise TypeError(error)
         new_matrix.append(buffer_row)
-        """For each row, it is cleaned"""
-        buffer_row = [] 
-    return (new_matrix)
+        buffer_row = []  # For each row, it is cleaned
+
+    return new_matrix
