6bc31eaeeb67313bebe48f419a29d335d498d2fe
jeremias <jeremiaserba7894@gmail.com>
2023-06-15 19:10:25 +0000

    Task 7 - Use of new variable in str method

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -72,8 +72,8 @@ class Rectangle:
             return ''
         """ subtract one to take out the space and print on the outside """
         for i in range(self.__height - 1):
-            print("#" * self.__width)
-        print('#' * self.__width, end="")
+            print(f"{ Rectangle.print_symbol }" * self.__width)
+        print(f' { Rectangle.print_symbol } ' * self.__width, end="")
         return ''
 
     def __repr__(self):
