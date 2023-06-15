574e8371714f7bf905e911a590d58ef9b4ad6c30
jeremias <jeremiaserba7894@gmail.com>
2023-06-15 14:52:57 +0000

    Task 1 - Add conditions in the construct method

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,6 +5,18 @@
 class Rectangle:
     """ A class representing a rectangle. """
     def __init__(self, width=0, height=0):
+        """Initializate the Rectangle."""
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
         self.__height = height
         self.__width = width
 
