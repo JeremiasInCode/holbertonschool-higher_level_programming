c9fde6bc3f5dd01f94d49e7ff81c14420baf039a
jeremias <jeremiaserba7894@gmail.com>
2023-06-15 14:46:19 +0000

    Task 1 - Fix in the order

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,16 +5,25 @@
 class Rectangle:
     """ A class representing a rectangle. """
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
-
-    @property
-    def width(self):
-        return self.__width
+        self.__width = width
 
     @property
     def height(self):
+        """Return the height of Rectangle as a property."""
         return self.__height
+    @property
+    def width(self):
+        """Return the width of Rectangle as a property."""
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise TypeError("width must be >= 0")
+        self.__height = value
 
     @width.setter
     def width(self, value):
@@ -24,10 +33,13 @@ class Rectangle:
             raise TypeError("width must be >= 0")
         self.__width = value
 
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise TypeError("width must be >= 0")
-        self.__height = value
+
+def main():
+    my_rectangle = Rectangle(2, 4)
+    print(my_rectangle.__dict__)
+
+    my_rectangle.width = 10
+    my_rectangle.height = 3
+    print(my_rectangle.__dict__)
+
+main()
