4c5e01793663576e4e36251f63d952066cc65399
jeremias <jeremiaserba7894@gmail.com>
2023-06-15 19:51:49 +0000

    Task 8 - Fix in the condition

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -90,11 +90,9 @@ class Rectangle:
         """ Return the bigger or equal rectangle """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
 
-        if Rectangle.area(rect_1) == Rectangle.area(rect_2):
+        if Rectangle.area(rect_1) >= Rectangle.area(rect_2):
             return rect_1
-        else:
-            return rect_2
+        return rect_2
