125e77b41fec8868fdfc0e8a5ea2203af5b405c6
jeremias <jeremiaserba7894@gmail.com>
2023-06-19 20:04:10 +0000

    Task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,14 +6,3 @@ class MyList(list):
     """ Prints the list, but sorted in ascending sort """
     def print_sorted(self):
         print(sorted(self))
-def main():
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-    print(my_list)
-    my_list.print_sorted()
-    print(my_list)
-main()
