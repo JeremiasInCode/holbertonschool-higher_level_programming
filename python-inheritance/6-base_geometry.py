8557720cb40d51c06986f285398e473c1616bac3
jeremias <jeremiaserba7894@gmail.com>
2023-06-20 17:24:38 +0000

    Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Task 6 """
+
+class BaseGeometry:
+      """ Creation of classes for future tasks """
+      def area(self):
+            """ Return Exception area is not implemented """
+            raise Exception("area() is not implemented")
