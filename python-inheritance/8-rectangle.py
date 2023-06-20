ec304226bbbbe618a67e686e9336a395e9bc1d18
jeremias <jeremiaserba7894@gmail.com>
2023-06-20 18:38:13 +0000

    Task 8 - Fix in called methods

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle(BaseGeometry):
     """ Class represented a rectangle. """
     def __init__(self, width, height):
         """ Initialize attributes and validations. """
-        self.integer_validator(width)
-        self.integer_validator(height)
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
