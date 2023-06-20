7da7bf8625913e1999bf7110dbc7613717f7d06e
jeremias <jeremiaserba7894@gmail.com>
2023-06-20 13:16:16 +0000

    Task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,7 +4,8 @@
 
 class MyList(list):
     """ Prints the list, but sorted in ascending sort """
+
     def print_sorted(self):
         if hasattr(self, '__str__'):
             print(sorted(self))
-            return(sorted(self))
+            return (sorted(self))
