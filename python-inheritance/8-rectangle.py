51f126cd63f037c55ea3a9ff1401f8795f75e2b1
jeremias <jeremiaserba7894@gmail.com>
2023-06-20 18:47:43 +0000

    Task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -11,3 +11,10 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+    
+    def area(self):
+        """ Return implementation of area """
+        return self.__width * self.__height
+
+    def str(self) -> str:
+        return (f"[Rectangle] {self.__width}/{self.__height}")
