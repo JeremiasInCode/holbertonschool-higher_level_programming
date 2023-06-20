635b4c556146eb432503f220590d6056beac69c4
jeremias <jeremiaserba7894@gmail.com>
2023-06-20 14:36:03 +0000

    Task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """Return true if obj inherits from a_class (subclass)"""
-    if obj is None or type(obj) == a_class:
+    if type(obj) == a_class:
         return False
     if issubclass(type(obj), a_class):
         return True
