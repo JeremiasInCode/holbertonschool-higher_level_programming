80677ebe748a7112f9038f1637ed2fd55768de78
jeremias <jeremiaserba7894@gmail.com>
2023-06-20 18:50:58 +0000

    Task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 """ Task 8 """
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """ Class represented a rectangle. """
     def __init__(self, width, height):
-        """ Initialize attributes and validations. """
-        self.integer_validator("width", width)
+        """Intialize a rectangule"""
+        self.integer_validator(width)
         self.__width = width
-        self.integer_validator("height", height)
+        self.integer_validator(height)
         self.__height = height
-
-    def area(self):
-        """ Return implementation of area """
-        return self.__width * self.__height
-
-    def str(self) -> str:
-        return (f"[Rectangle] {self.__width}/{self.__height}")
