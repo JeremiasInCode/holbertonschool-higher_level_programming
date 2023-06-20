bb38c41c2ae335dc052ba5323e9ef6e6c65aee80
jeremias <jeremiaserba7894@gmail.com>
2023-06-20 18:41:26 +0000

    Task 8 - Fix error by pycodestyle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,6 +2,7 @@
 """ Task 8 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """ Class represented a rectangle. """
     def __init__(self, width, height):
