fdada15fcd79c1420d677d90681fdf9445fdc4bc
JeremiasInCode <jeremiaserba7994@gmail.com>
2023-06-21 13:21:31 -0300

    Task 6 - Fix in the open

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,6 +5,6 @@ import json
 
 def load_from_json_file(filename):
     """ To creates a object from Json file """
-    with open(filename, 'r') as f:
+    with open(filename) as f:
         data_json = json.loads(f)
     return data_json
