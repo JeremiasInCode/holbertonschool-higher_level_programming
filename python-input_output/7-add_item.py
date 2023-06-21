ca5afb0eaa4b7d95fd48d05c6d84e8f4a594d551
JeremiasInCode <jeremiaserba7994@gmail.com>
2023-06-21 14:18:55 -0300

    Task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Task 7 """
+import json
+""" arguments """
+import sys
+
+save_to_json_file = __import__('save_to_json_file').save_to_json_file
+load_from_json_file = __import__('load_from_json_file').load_from_json_file
+
+try:
+    with open('add_item.json') as f:
+        data_obj = load_from_json_file('add_item.json')
+except FileNotFoundError:
+    data_obj = []
+
+data_obj.extends(sys)
+save_to_json_file(data_obj, 'add_item.json')
