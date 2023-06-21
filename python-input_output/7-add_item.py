43153f98ee6334241a7bc1fc30128af0efe16878
JeremiasInCode <jeremiaserba7994@gmail.com>
2023-06-21 14:24:02 -0300

    Task 7 - Fix in use of arguments

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,6 +6,7 @@ import sys
 
 save_to_json_file = __import__('save_to_json_file').save_to_json_file
 load_from_json_file = __import__('load_from_json_file').load_from_json_file
+args = sys.argv[1:]
 
 try:
     with open('add_item.json') as f:
