f89793d7daff36956e6ecf918374c81cca4f25f6
jeremias <jeremiaserba7894@gmail.com>
2023-06-23 15:03:18 +0000

    Task 9 - Change in name of attribute

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,12 +5,12 @@
 class Student:
     def __init__(self, first_name, last_name, age):
         """ Initialization of attributes """
-        self.firt_name = first_name
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
         def to_json(self):
-            """Return a -representation- of a dictionary of Student class"""
+            """Return a JSON representation of a dictionary of Student class"""
             return {
                 "first_name": self.first_name,
                 "last_name": self.last_name,
