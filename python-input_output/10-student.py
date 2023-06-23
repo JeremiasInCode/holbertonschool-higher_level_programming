ddbe0c8b879c4174c7efc6270173ce1b23e4b549
jeremias <jeremiaserba7894@gmail.com>
2023-06-23 15:51:11 +0000

    Task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Task 10 """
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """ Initialization of attributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a JSON representation of a dictionary of Student class"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for element in attrs:
+                if hasattr(self, element):
+                    new_dict[element] = getattr(self, element)
+            return new_dict
