26b3c0170b170fa9dc30280258f66814d4c84303
jeremias <jeremiaserba7894@gmail.com>
2023-06-23 15:51:01 +0000

    Task 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -9,13 +9,10 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
+    def to_json(self):
         """Return a JSON representation of a dictionary of Student class"""
-        if attrs is None:
-            return self.__dict__
-        else:
-            new_dict = {}
-            for element in attrs:
-                if hasattr(self, element):
-                    new_dict[element] = getattr(self, element)
-            return new_dict
+        return {
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "age": self.age
+        }
