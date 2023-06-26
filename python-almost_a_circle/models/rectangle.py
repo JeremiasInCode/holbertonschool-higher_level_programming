5c3a290971f4d88d79edd4200c7d10ea8a831539
jeremias <jeremiaserba7894@gmail.com>
2023-06-26 19:58:11 +0000

    Task 2 - Fix in confition under o equal to 0

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -25,7 +25,7 @@ class Rectangle(Base):
         """ Sets the width of the Rectangle. """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if self.__width <= 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -42,7 +42,7 @@ class Rectangle(Base):
         """ Sets the height of the Rectangle. """
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if self.__width <= 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
@@ -59,7 +59,7 @@ class Rectangle(Base):
         """ Sets x of the Rectangle. """
         if type(value) is not int:
             raise TypeError("x must be an integer")
-        if self.__width <= 0:
+        if value <= 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
@@ -79,6 +79,6 @@ class Rectangle(Base):
         """
         if type(value) is not int:
             raise TypeError("y must be an integer")
-        if self.__width <= 0:
+        if value <= 0:
             raise ValueError("y must be >= 0")
         self.__y = value
