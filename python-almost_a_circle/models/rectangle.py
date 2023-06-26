004d16ecd80eda1b99acdb089d03541a54d41e84
jeremias <jeremiaserba7894@gmail.com>
2023-06-26 19:28:35 +0000

    Task 2 - Add rectangle class

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Task 2"""
+Base = __import__("base.py").Base
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """
+        Private instance attributes,
+        Return width of the Rectangle as a property.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Sets the width of the Rectangle. """
+        pass
+
+    @property
+    def height(self):
+        """
+        Private instance attributes,
+        Return height of the Rectangle as a property.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Sets the height of the Rectangle. """
+        pass
+
+    @property
+    def x(self):
+        """
+        Private instance attributes,
+        Return x of the Rectangle as a property.
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Sets x of the Rectangle. """
+
+    @property
+    def y(self, value):
+        """
+        Private instance attributes,
+        Return x of the Rectangle as a property.
+        """
+        return self.__y
