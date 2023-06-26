1fab8199a7f2ecb27f7e06119f8c66509d6f94b0
jeremias <jeremiaserba7894@gmail.com>
2023-06-26 21:01:46 +0000

    Task 6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -93,3 +93,7 @@ class Rectangle(Base):
             print()
         for element in range(self.height):
             print(' ' * self.x + '#' * self.width)
+
+    def __str__(self):
+        aux = {self.width}/{self.height}
+        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - {aux}")
