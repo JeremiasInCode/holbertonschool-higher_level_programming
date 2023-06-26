15146357f4baddbbf7ffd623235c15ec75452d01
jeremias <jeremiaserba7894@gmail.com>
2023-06-26 20:39:39 +0000

    Task 5 - Impor

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Task 3"""
+from models.base import Base
 
-
-class Rectangle():
+class Rectangle(Base):
     """ Initialize an instance """
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
