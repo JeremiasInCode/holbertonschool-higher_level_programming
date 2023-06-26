f9df9f1e6e17579ecbd3fe37632a3def943833b4
jeremias <jeremiaserba7894@gmail.com>
2023-06-26 19:34:29 +0000

    Fix import

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Task 2"""
-Base = __import__("base.py").Base
+from models.base import Base
 
 
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initialize an instance """
         super().__init__(id)
         self.__width = width
         self.__height = height
