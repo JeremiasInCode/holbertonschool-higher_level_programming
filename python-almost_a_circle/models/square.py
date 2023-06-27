e97dd86e74ca967977284138b1f35573b4d495ab
jeremias <jeremiaserba7894@gmail.com>
2023-06-27 17:18:51 +0000

    Task 12 - Square class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -27,3 +27,16 @@ class Square(Rectangle):
         """Sets the width of the Square."""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """ Assigns an argument to each attribute """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.size = args[1]
+        if len(args) >= 3:
+            self.x = args[2]
+        if len(args) >= 4:
+            self.y = args[3]
+        for key, value in kwargs.items():
+            setattr(self, key, value)
