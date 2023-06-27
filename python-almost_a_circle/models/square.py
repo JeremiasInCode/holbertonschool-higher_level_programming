8593fb49f811d41f4c6412770fbb61c9fe08168f
jeremias <jeremiaserba7894@gmail.com>
2023-06-27 17:32:46 +0000

    Task 13 - Add comments

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,6 +6,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """ Initialize the instance """
     def __init__(self, size, x=0, y=0, id=None):
+        """ inherit from rectangle """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
