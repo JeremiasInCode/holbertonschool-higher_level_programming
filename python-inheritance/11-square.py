7d2639dd1e0e7d2651c9e702c5ff412832402cde
JereDev19 <jeremiaserba7894@gmail.com>
2023-10-03 15:19:26 -0300

    Task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Task 11 """
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+    
+    def area(self):
+        return super().area();
+
+    def __str__(self) -> str:
+        return (f"[Square] {self.__size}/{self.__size}")
