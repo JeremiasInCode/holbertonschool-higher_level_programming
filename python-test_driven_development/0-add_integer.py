dd580412af4e3270466c4f14a514d8965c156de1
Jeremias Erba <jeremiaserba7894@gmail.com>
2023-10-03 11:16:29 -0300

    Fix error of spaces

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-""" Task 0 """
+"""Task 0"""
+
+
 def add_integer(a, b=98):
     """Add two integers"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    
+
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
