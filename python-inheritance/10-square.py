5971544fc0200213fe4d4d888ea92debf1f663bb
JereDev19 <jeremiaserba7894@gmail.com>
2023-10-03 14:48:57 -0300

    Task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Task 9 """
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class represented a rectangle. """
+    def __init__(self, size):
+        """Intialize a rectangule"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ Return implementation of area """
+        return self.__size ** 2
