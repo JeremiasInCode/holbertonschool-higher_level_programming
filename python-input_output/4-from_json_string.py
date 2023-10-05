c5f123a4f725325018819915501a7d362c47e6b9
JereDev19 <jeremiaserba7894@gmail.com>
2023-10-05 11:41:15 -0300

    Fixed task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Task 3 """
+""" Task 4 """
 import json
 
 
 def from_json_string(my_str):
     """ Json to a object (deserealize) """
-    json.loads(my_str)
+    return json.loads(my_str)
