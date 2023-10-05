f6aee342268e1c8d83cf549bf61e318f56aa91b6
JereDev19 <jeremiaserba7894@gmail.com>
2023-10-05 11:30:06 -0300

    Task 12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Task 12 """
+
+
+def pascal_triangle(n):
+    """Implement pascal triangle in a algoritm"""
+
+    triangle = []
+    row = [1]
+    cero = [0]
+
+    concat1 = row + cero
+    concat2 = cero + row
+
+    """ concat in pairs """
+    pairs = zip(concat1, concat2)
+    """ Add these pairs to create the row """
+    add = [element + d for element, d in pairs] 
+    triangle.append(add)
+    return triangle
