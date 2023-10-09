4ea8b59967e19816f40ec2335da1f76d05052050
JereDev19 <jeremiaserba7894@gmail.com>
2023-10-09 19:12:50 -0300

    Tests

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" Test for base class """
+import unittest, os
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """ Test base class """
+    def test_id(self):
+        """ id test """
+        obj1 = Base()
+        self.assertEqual(obj1.id, 1)
+        obj2 = Base()
+        self.assertEqual(obj2.id, 2)
+        obj3 = Base(10)
+        self.assertEqual(obj3.id, 10)
+        obj4 = Base(-20)
+        self.assertEqual(obj4, -20)
+
+    def test_to_json_string(self):
+        """ convert list to json - test """
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+        self.assertEqual(Base.to_json_string([{"id": 1}]), {"id": 1})
+        self.assertEqual(Base.to_json_string([False]), False)
+
+    def test_from_json_string(self):
+        """ convert json to list - test"""
+        self.assertAlmostEqual(Base.from_json_string(None), [])
+        self.assertAlmostEqual(Base.from_json_string("[]"), [])
+        test = Base.from_json_string('[{ "id": 30 }]')
+        self.assertAlmostEqual(test, [{"id": 30}])
+
+    def test_save_to_file(self):
+        """test save_to_file."""
+        r1 = Base.save_to_file(None)
+        self.assertEqual(Base.load_from_file(), [])
+        os.remove("./Square.json")
+        r1 = Base.save_to_file([])
+        self.assertEqual(Base.load_from_file(), [])
+        os.remove("./Square.json")
+        r1 = Base.save_to_file([Base.Square(1)])
+        self.assertIsInstance(Base.load_from_file()[0], Base.Square)
+        os.remove("./Square.json")
+        r1 = Base.load_from_file()
+        self.assertEqual(Base.save_to_file(r1), None)
+
+    if __name__ == "__main__":
+        unittest.main()
---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,38 @@
+import unittest
+from models.rectangle import Rectangle
+from io import StringIO
+import sys
+
+class TestRectangle(unittest.TestCase):
+    """ Test rectangle class """
+    def setUp(self):
+        self.rectangle1 = Rectangle(5, 10)
+        self.rectangle2 = Rectangle(7, 3, 2, 4, 1)
+
+    """ Test area function """  
+    def test_area(self):
+        self.assertEqual(self.rectangle1.area(), 50)
+        self.assertEqual(self.rectangle2.area(), 21)
+
+    """ Test display function """
+    def test_display(self):
+        rectangle_dis = Rectangle(5, 10)
+        captured_output = StringIO()
+        sys.stdout = captured_output    
+        rectangle_dis.display()
+        sys.stdout = sys.__stdout__
+
+        output = captured_output.getvalue().strip()
+        expected_output = "#####\n" * 10
+        self.assertEqual(output, expected_output)
+
+    def test_update(self):
+        self.rectangle1.update(2, 8, 6, 1, 2)
+        self.assertEqual(self.rectangle1.width, 8)
+        self.assertEqual(self.rectangle1.height, 6)
+        self.assertEqual(self.rectangle1.x, 1)
+        self.assertEqual(self.rectangle1.y, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
