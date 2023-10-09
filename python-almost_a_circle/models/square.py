a2d88559609ddc21a4c005dcfdf5a1cdf3b1e1d9
JereDev19 <jeremiaserba7894@gmail.com>
2023-10-09 19:39:08 -0300

    Square tasks

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,3 +11,32 @@ class Square(Rectangle):
     def __str__(self):
         """ Print [Square] (<id>) <x>/<y> - <size> """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    """ getter """
+    @property
+    def size(self):
+        """
+        Private instance attributes,
+        Return width of the Rectangle as a property.
+        """
+        return self.width
+
+    """ setter """
+    @size.setter
+    def size(self, value):
+        """Sets the width of the Square."""
+        self.width = value
+        self.height = value
+    
+    def update(self, *args, **kwargs):
+        """ Assigns an argument to each attribute """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.size = args[1]
+        if len(args) >= 3:
+            self.x = args[2]
+        if len(args) >= 4:
+            self.y = args[3]
+        for key, value in kwargs.items():
+            setattr(self, key, value)
