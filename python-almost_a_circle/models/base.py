81b698a2b5faf7eefdd33318cbdddfa69f68c92b
Jeremias Erba <jeremiaserba7894@gmail.com>
2023-10-10 17:13:37 -0300

    Task 19

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -68,3 +68,20 @@ class Base:
         dummy_instance = cls(1, 1) if cls.__name__ == "Rectangle" else cls(1)
         dummy_instance.update(**dictionary)
         return dummy_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """ returns a list of instances """
+        name_class = cls.__name__ + '.json'
+        instance_list = []
+        try:
+            with open(name_class, "r") as file:
+                json_data = file.read()
+                if json_data:
+                    dictionary_data = cls.from_json_string(json_data)
+                    for directory in dictionary_data:
+                        instance = cls.create(**directory)
+                        instance_list.append(instance)
+        except FileNotFoundError:
+            return []
+        return instance_list
