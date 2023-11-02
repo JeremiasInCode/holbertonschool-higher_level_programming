3caf5a8e0c6a672f8e891f33c2330b011362504b
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-02 11:33:50 -0300

    Task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-"""
-@author: JeremiasInCode
-@classes:
-Rectangle:
-    In this class is for representing an empty rectangle.
-"""
 
 
 class Square:
-    """Empty class for Square."""
+    """A class representing a square."""
+
     pass
