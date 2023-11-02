27d8a134871ab1b088af36d582a92b8a59426a32
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-02 11:26:21 -0300

    Task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,12 +6,18 @@ class Square:
     error = "position must be a tuple of 2 positive integers"
 
     def __init__(self, size=0, position=(0, 0)):
+        """ Initialize an instance of Square. """
+
         self.size = size
         self.position = position
 
     @property
     def size(self):
         return self.__size
+
+    @property
+    def position(self):
+        return self.__position
 
     @size.setter
     def size(self, value):
@@ -22,12 +28,9 @@ class Square:
         else:
             raise TypeError("size must be an integer")
 
-    @property
-    def position(self):
-        return self.__position
-
     @position.setter
     def position(self, value):
+        """Set the position of the Square."""
         if len(value) == 2 and isinstance(value, tuple):
             x = value
             y = value
@@ -39,6 +42,7 @@ class Square:
             raise TypeError(Square.error)
 
     def area(self):
+        "This function to calulate the area of a square"
         return self.__size ** 2
 
     def my_print(self):
@@ -50,7 +54,8 @@ class Square:
         for element in range(self.__position[1]):
             print("")
         for i in range(self.__size):
-            for i in range(self.__position[0]):
+            for i in range(self.position[0]):
                 print(" ", end="")
-            print("#", end="")
-        print("")
+            for j in range(self.size):
+                print("#", end="")
+            print("")
