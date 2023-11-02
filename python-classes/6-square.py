7012993b015cef28ee600637c8e791d83f4742c2
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-02 11:31:38 -0300

    Add validation of setter in construct

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -3,28 +3,54 @@
 
 
 class Square:
+    """ A class representing a square. """
+
+    # error by betty.
     error = "position must be a tuple of 2 positive integers"
 
     def __init__(self, size=0, position=(0, 0)):
         """ Initialize an instance of Square. """
 
-        self.size = size
-        self.position = position
+        if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+        if isinstance(position, tuple):
+            try:
+                if (isinstance(position[0], int) and
+                        isinstance(position[1], int)):
+                    if position[0] >= 0 and position[1] >= 0:
+                        self.__position = position
+                    else:
+                        raise ValueError(Square.error)
+                else:
+                    raise TypeError(Square.error)
+            except IndexError:
+                print(Square.error)
+        else:
+            raise TypeError(Square.error)
 
     @property
     def size(self):
+        """ Return the size of the Square as a property """
         return self.__size
 
     @property
     def position(self):
+        """ Return the specific position of the Square as a property """
         return self.__position
 
     @size.setter
     def size(self, value):
-        if isinstance(value, int):
+        if type(value) == int:
             if value < 0:
                 raise ValueError("size must be >= 0")
-            self.__size = value
+            else:
+                self.__size = value
         else:
             raise TypeError("size must be an integer")
 
@@ -47,7 +73,8 @@ class Square:
 
     def area(self):
         "This function to calulate the area of a square"
-        return self.__size ** 2
+        area = self.__size ** 2
+        return area
 
     def my_print(self):
         """ prints in stdout the square with the character # """
@@ -57,9 +84,9 @@ class Square:
             return
         for element in range(self.__position[1]):
             print("")
-        for i in range(self.__size):
-            for i in range(self.position[0]):
+        for element1 in range(self.__size):
+            for element1 in range(self.position[0]):
                 print(" ", end="")
-            for j in range(self.size):
+            for element3 in range(self.size):
                 print("#", end="")
             print("")
