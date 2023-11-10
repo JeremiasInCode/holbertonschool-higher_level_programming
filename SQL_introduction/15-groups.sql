1268b19c57e011c1cf9cb24c1004456c88038b57
Jeremias Erba <jeremiaserba7894@gmail.com>
2023-11-09 21:42:50 -0300

    Task 15 - Fix error

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score, count(score) as number FROM second_table group by score desc;
+SELECT score, count(score) as number FROM second_table group by score ORDER BY score desc;
