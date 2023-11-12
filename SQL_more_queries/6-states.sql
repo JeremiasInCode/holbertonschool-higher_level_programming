4e7fc5447b935a6674ec68ca18eca39b19d3806f
Jeremias Erba <jeremiaserba7894@gmail.com>
2023-11-11 21:37:05 -0300

    Task 6 - add comments and fix error

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- Creates the database hbtn_0d_usa and the table states
 DROP DATABASE IF EXISTS hbtn_0d_usa;
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT UNIQUE auto_increment not null PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
