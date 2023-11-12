76c57828d88cc85dd92415d20e3701890545f59c
Jeremias Erba <jeremiaserba7894@gmail.com>
2023-11-12 14:15:13 -0300

    Task 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,4 @@
+-- Select the id and name of the cities that have california as name
 SELECT id, name FROM cities WHERE state_id = (
     SELECT id FROM states WHERE name = 'California'
 ) ORDER BY id;
