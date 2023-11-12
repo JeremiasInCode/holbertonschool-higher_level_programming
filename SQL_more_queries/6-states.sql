063b3ee2a4034a1678a2a6318b125d6ed1e77825
Jeremias Erba <jeremiaserba7894@gmail.com>
2023-11-11 21:33:20 -0300

    Task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+DROP DATABASE IF EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE auto_increment not null PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
