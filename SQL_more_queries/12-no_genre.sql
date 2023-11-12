65e87d08f8dc13071bb957b9a15f1f8260990c7e
Jeremias Erba <jeremiaserba7894@gmail.com>
2023-11-12 14:56:53 -0300

    Task 12

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -2,4 +2,4 @@
 -- list all shows contained in  hbtn_0d_tvshows without a genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id FROM cities
 LEFT JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
+ORDER BY tv_shows.title, tv_show_genres.genre_id
