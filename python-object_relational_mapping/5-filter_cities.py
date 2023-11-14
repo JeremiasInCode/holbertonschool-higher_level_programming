81d5396cb2ea097a072b70475609db638aa4208c
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-14 19:29:45 -0300

    Task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -2,6 +2,7 @@
 "This script lists all states starting with an 'N' from the db 'hbtn_0e_usa'"
 import MySQLdb
 import sys
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3],
