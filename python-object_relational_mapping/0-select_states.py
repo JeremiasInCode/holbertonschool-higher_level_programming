3dd64caceeee7b2b72cdcff46181e8da92e7a580
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-14 11:58:20 -0300

    Add permissions

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     )
 
     db_cursor = db_conn.cursor()
-    db_cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    db_cursor.execute("SELECT * FROM states")
     rows_selected = db_cursor.fetchall()
 
     db_conn.close()
