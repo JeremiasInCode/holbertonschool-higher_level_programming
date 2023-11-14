fb1bfdc2bd527c5dd882e5ed62fa3fef15d7de37
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-14 19:19:32 -0300

    Task 1 - end line

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,26 +1,42 @@
 #!/usr/bin/python3
-"""lists all states"""
+"""
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa, but takes the state name as an
+argument from the command line.
+"""
 
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    db_conn = MySQLdb.connect(
-        host="localhost",
-        user=argv[1],
-        port=3306,
-        password=argv[2],
-        database=argv[3]
-    )
-    db_cursor = db_conn.cursor()
-    db_cursor.execute (
-        "SELECT * FROM states\
-        WHERE name LIKE BINARY 'N%'\
-        ORDER BY states.id ASC"
-    )
-    rows_selected = db_cursor.fetchall()
-    for item in rows_selected:
-        print(item)
-    db_cursor.close()
-    db_conn.close()
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database)
+
+    # Create a cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # Execute the SQL query to select states
+    # with names starting with the given input
+    cursor.execute("SELECT * FROM states WHERE name \
+                   LIKE BINARY %s ORDER BY id ASC", (state_name,))
+
+    # Fetch all the rows returned by the query
+    rows = cursor.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
