b9229dd0fe643c56cefcdabdba2b14bbcd161a9c
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-14 10:45:26 -0300

    Task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""lists all states"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """./0-select_states.py root root hbtn_0e_0_usa"""
+
+    db_conn = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        port=3306,
+        password=argv[2],
+        database=argv[3]
+    )
+
+    db_cursor = db_conn.cursor()
+    db_cursor.execute (
+        "SELECT * FROM states \
+        WHERE name LIKE BINARY 'N%' \
+        ORDER BY states.id ASC"
+        
+    )
+    rows_selected = db_cursor.fetchall()
+
+    db_conn.close()
+
+    for item in rows_selected:
+        print(item)
