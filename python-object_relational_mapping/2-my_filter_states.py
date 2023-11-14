5a5bc5180283909f494c84a7e11ad0724e34c79b
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-14 10:57:52 -0300

    Task 2 - add comments

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-"""lists all states"""
+""" takes in an argument and displays all values in
+    the states table of hbtn_0e_0_usa where name matches the argument"""
 
 import MySQLdb
 from sys import argv
 
 
 if __name__ == "__main__":
-    """./0-select_states.py root root hbtn_0e_0_usa"""
+    """./2-my_filter_states.py root root hbtn_0e_0_usa 'Arizona'"""
 
     db_conn = MySQLdb.connect(
         host="localhost",
         user=argv[1],
         port=3306,
         password=argv[2],
-        database=argv[3]
+        db=argv[3]
     )
 
     db_cursor = db_conn.cursor()
-    db_cursor.execute (
+    db_cursor.execute(
         "SELECT * FROM states\
-        WHERE name LIKE BINARY '{}'\
-        ORDER BY states.id ASC".format(argv[4])
-        
+         WHERE name LIKE BINARY '{}'\
+         ORDER BY states.id ASC".format(argv[4])
     )
     rows_selected = db_cursor.fetchall()
 
     db_conn.close()
 
-    for item in rows_selected:
-        print(item)
+    for r in rows_selected:
+        print(r)
