a531b6022aae41006552bd85eda64cca917d1dc8
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-14 18:50:57 -0300

    Task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" takes in an argument and displays all values in
+    the states table of hbtn_0e_0_usa where name matches the argument"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """./2-my_filter_states.py root root hbtn_0e_0_usa 'Arizona'"""
+
+    db_conn = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        port=3306,
+        password=argv[2],
+        db=argv[3]
+    )
+
+    db_cursor = db_conn.cursor()
+    db_cursor.execute("SELECT * FROM states WHERE\
+                    BINARY name = %(name)s", {'name': sys.argv[4]})
+    rows_selected = db_cursor.fetchall()
+    for r in rows_selected:
+        print(r)
+    db_cursor.close()
+    db_conn.close()
