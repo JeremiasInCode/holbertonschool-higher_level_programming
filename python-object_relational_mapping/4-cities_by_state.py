f35ab492c9183cd1fdf0fc92ca8892dbd1b98ef2
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-14 19:10:55 -0300

    Task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" lists all cities from the database hbtn_0e_4_usa"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """./4-my_filter_states.py root root hbtn_0e_0_usa 'Arizona'"""
+
+    db_conn = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        port=3306,
+        password=argv[2],
+        db=argv[3]
+    )
+
+    db_cursor = db_conn.cursor()
+    db_cursor.execute("SELECT id, cities.name, states.name FROM cities "
+                        + "INNER JOIN states ON states.id = cities.state_id")
+    rows = db_cursor.fetchall()
+    for row in rows:
+        print(row)
