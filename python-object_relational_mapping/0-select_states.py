762d57933aa7df1b3412c2f224a703bfde428bf9
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-14 10:25:32 -0300

    Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 ''' 0-select_states module '''
 
-
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
     db = MySQLdb.connect(
-        host = 'localhost',
-        port = 3306,
-        user = argv[1],
-        password = argv[2],
-        database = argv[3] 
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
     )
-
     cursor = db.cursor()
-    cursor.execute(""" select * from states """);
-    states_list = cursor.fetchall()
-    for item in states_list:
+    cursor.execute("""SELECT * FROM states""")
+    statesList = cursor.fetchall()
+    for item in statesList:
         print(item)
     db.close()
