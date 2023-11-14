e1cbce83eab2e691d84f48d77e342e3877d4a506
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-14 19:19:45 -0300

    Fixes task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,44 @@
 #!/usr/bin/python3
-""" takes in an argument and displays all values in
-    the states table of hbtn_0e_0_usa where name matches the argument"""
-if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
+"""
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+But this time, it is safe from MySQL injections!
+"""
 
-    db_conn = MySQLdb.connect(
-        host="localhost",
-        user=argv[1],
-        port=3306,
-        password=argv[2],
-        db=argv[3]
-    )
-    db_cursor = db_conn.cursor()
-    db_cursor.execute("SELECT states.id, states.name FROM states".format())
-    rows_selected = db_cursor.fetchall()
-    for r in rows_selected:
-        if r[1] == sys.argv[4]:
-            print(r)
-    db_cursor.close()
-    db_conn.close()
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Get command line argument
+    search_name = sys.argv[1]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user="username",
+                         passwd="password",
+                         db="database")
+
+    # Create a cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # Use the escape_string method to
+    # sanitize the input and prevent SQL injection
+    sanitized_name = MySQLdb.escape_string(search_name)
+
+    # Execute the SQL query to select states matching the sanitized input
+    query = "SELECT * FROM states WHERE name \
+    LIKE BINARY '{}' ORDER BY id ASC".format(sanitized_name)
+
+    cursor.execute(query)
+
+    # Fetch all the rows returned by the query
+    rows = cursor.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
