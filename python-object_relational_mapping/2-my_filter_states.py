dbb0d2115631561a61b755c6200680a00b23d5ae
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-14 19:10:19 -0300

    Last fix task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 """ takes in an argument and displays all values in
     the states table of hbtn_0e_0_usa where name matches the argument"""
-
-import MySQLdb
-from sys import argv
-
-
 if __name__ == "__main__":
-    """./2-my_filter_states.py root root hbtn_0e_0_usa 'Arizona'"""
+    import MySQLdb
+    from sys import argv
 
     db_conn = MySQLdb.connect(
         host="localhost",
@@ -16,16 +12,11 @@ if __name__ == "__main__":
         password=argv[2],
         db=argv[3]
     )
-
     db_cursor = db_conn.cursor()
-    db_cursor.execute(
-        "SELECT * FROM states\
-         WHERE name LIKE BINARY '{}'\
-         ORDER BY states.id ASC".format(argv[4])
-    )
+    db_cursor.execute("SELECT states.id, states.name FROM states".format())
     rows_selected = db_cursor.fetchall()
-
-    db_conn.close()
-
     for r in rows_selected:
-        print(r)
+        if r[1] == sys.argv[4]:
+            print(r)
+    db_cursor.close()
+    db_conn.close()
