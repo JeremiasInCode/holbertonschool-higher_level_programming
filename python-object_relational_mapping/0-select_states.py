c16131d0265f996d2a1cccbec4a17f531431e942
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-14 18:33:43 -0300

    Task 0 - Fixes

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """lists all states"""
-
 import MySQLdb
 from sys import argv
 
 
 if __name__ == "__main__":
     """./0-select_states.py root root hbtn_0e_0_usa"""
-
     db_conn = MySQLdb.connect(
         host="localhost",
-        user=argv[1],
         port=3306,
+        user=argv[1],
         password=argv[2],
         database=argv[3]
     )
-
     db_cursor = db_conn.cursor()
-    db_cursor.execute("SELECT * FROM states")
-    rows_selected = db_cursor.fetchall()
+    query = "SELECT * FROM states ORDER BY id"
+    db_cursor.execute(query)
+    states = db_cursor.fetchall()
 
+    for state in states:
+        print(state)
     db_conn.close()
-
-    for item in rows_selected:
-        print(item)
