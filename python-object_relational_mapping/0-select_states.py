8f8d4f82f7122966bd80d700079f6c87d168ceed
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-15 16:38:45 -0300

    Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,40 +1,15 @@
 #!/usr/bin/python3
-
-"""
-Script that lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa.
-"""
-
-import sys
+"This script lists all states from the db 'hbtn_0e_usa'"
 import MySQLdb
-
-
+import sys
 if __name__ == "__main__":
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3],
+                         port=3306)
+    cur = db.cursor()
 
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=database)
-
-    # Create a cursor object to execute SQL queries
-    cursor = db.cursor()
-
-    # Execute the SQL query to select states with names starting with 'N'
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    # Fetch all the rows returned by the query
-    rows = cursor.fetchall()
-
-    # Print the results
-    for row in rows:
-        print(row)
-
-    # Close the cursor and database connection
-    cursor.close()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    fetchs = cur.fetchall()
+    for x in fetchs:
+        print(x)
     db.close()
