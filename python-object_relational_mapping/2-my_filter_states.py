985a9876c16fc2bddde55a30421207321126fdfe
JeremiasInCode <jeremiaserba7894@gmail.com>
2023-11-19 16:19:39 -0300

    Task 2 - fixess

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-"""Filter states by user input."""
+"script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3],
+                         port=3306)
 
+    db_cursor = db.cursor()
+    sn_searched=sys.argv[4]
 
-if __name__ == '__main__':
-    import MySQLdb
-    from sys import argv
+    db_cursor.execute("SELECT id, name FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(sys.argv[4]))
 
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-    name = argv[4]
+    q_rows = db_cursor.fetchall()
+    for i in q_rows:
+        print(i)
 
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY '{}'
-                ORDER BY states.id ASC""".format(name))
-    states = cur.fetchall()
-
-    for row in states:
-        print(row)
-
-    cur = cur.close()
-    db = db.close()
+    db_cursor.close()
+    db.close()
