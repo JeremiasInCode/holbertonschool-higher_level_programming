2e8874f245a88c67ae277d46d85604344db6ee50
JeremiasInCode <jeremiaserba7894@gmail.com>
2023-11-19 16:54:18 -0300

    Task 1, 2, 3 and 4

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"script that lists all states with a name"
+"script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"
 import MySQLdb
 import sys
-
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3],
                          port=3306)
 
     db_cursor = db.cursor()
-    sn_searched = sys.argv[4]
+    sn_searched=sys.argv[4]
 
     s_name = sys.argv[4]
     my_query = ("SELECT * FROM states WHERE name = %s ORDER BY id")
--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,19 +1,18 @@
-#!/usr/bin/env python3
-"This script lists all states starting with an 'N' from the db 'hbtn_0e_usa'"
+#!/usr/bin/python3
+"script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"
 import MySQLdb
 import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3],
                          port=3306)
-    cur = db.cursor()
-    cur.execute('''SELECT cities.id, cities.name, states.name
-                FROM cities
-                JOIN states
-                ON state_id = states.id
-                ORDER BY cities.id''')
 
-    fetchs = cur.fetchall()
-    for x in fetchs:
-        print(x)
+    db_cursor = db.cursor()
+    db_cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+
+    q_rows = db_cursor.fetchall()
+    for i in q_rows:
+        print(i)
+
+    db_cursor.close()
     db.close()
