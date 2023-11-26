f3aeacd5f89ff86c0c56021fd7e61197c30b6f72
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-26 19:30:30 -0300

    Task 2 - fixes

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Script that takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument"""
+states table"""
 
 if __name__ == "__main__":
     import MySQLdb
