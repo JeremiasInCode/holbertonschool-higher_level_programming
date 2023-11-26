890fc9fc04437825f01deed9c420577dbd2c5b82
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-26 19:04:43 -0300

    tasks

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -4,6 +4,7 @@
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
+
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
