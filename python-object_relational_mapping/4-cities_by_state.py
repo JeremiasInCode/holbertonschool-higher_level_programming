afb9e22329e1ad24f502ca988564eeaddff9b0ca
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-26 19:01:32 -0300

    Task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"
-import MySQLdb
-import sys
+"""Script that lists all cities from the database hbtn_0e_4_usa"""
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3],
-                         port=3306)
-
-    db_cursor = db.cursor()
-    db_cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
-
-    q_rows = db_cursor.fetchall()
-    for i in q_rows:
-        print(i)
-
-    db_cursor.close()
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    query = ("""SELECT cities.id, cities.name, states.name FROM cities
+            JOIN states ON cities.state_id = states.id ORDER BY id;""")
+    cur.execute(query)
+    states = cur.fetchall()
+    for state in states:
+        print(state)
     db.close()
