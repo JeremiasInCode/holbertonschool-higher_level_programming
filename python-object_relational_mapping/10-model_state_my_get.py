e841f50b73f70616ec31c6c0af90b1cb4139cfc0
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-26 19:15:22 -0300

    Ya no seeeeeeeeee

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -6,6 +6,7 @@ from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == "__main__":
     engine = create_engine(
         f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}'
