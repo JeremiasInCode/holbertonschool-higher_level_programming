a430cfeba4de9ed052b89f1ee0bef979a50734c8
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-26 18:19:11 -0300

    Task 7 - add permissions

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
     engine = create_engine(
         f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}'
