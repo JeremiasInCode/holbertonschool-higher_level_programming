e185bd73454ff4aa7a162d5393b39b09b842e066
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-26 18:15:17 -0300

    Task 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script that lists States from the database hbtn_0e_6_usa"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}'
+    )
+    Session = sessionmaker(engine)
+    session = Session()
+    states = session.query(State).order_by(State.id)
+    for state in states:
+        print(f'{state.id}: {state.name}')
+    session.close()
