2c7f04f280bef9935b1d2d3ae918ed8cd123633f
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-27 08:40:31 -0300

    Fixes

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,7 +2,6 @@
 """ Task 7 """
 import sys
 
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 args = sys.argv[1:]
@@ -12,6 +11,9 @@ try:
         data_obj = load_from_json_file('add_item.json')
 except FileNotFoundError:
     data_obj = []
+except Exception as e:
+    print(f"Error loading data from JSON file: {e}")
+    sys.exit(1)
 
 data_obj.extend(args)
 save_to_json_file(data_obj, 'add_item.json')
