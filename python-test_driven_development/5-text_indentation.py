24645f217505634c5fbfcb7105bc0abb44459770
JereDev19 <jeremiaserba7894@gmail.com>
2023-11-27 08:49:53 -0300

    Task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Task 5"""
+"""Task 5. Text indentation"""
 
 
 def text_indentation(text):
-    error = "text must be a string"
-
+    """
+    Print a text with 2 new lines after each of these characters: ., ? and :
+    """
     if not isinstance(text, str):
-        raise TypeError(error)
+        raise TypeError('text must be a string')
 
-    matches = [".", "?", ":"]
+    delimiters = {'.', '?', ':'}
     i = 0
 
     while i < len(text):
-        if text[i] in matches:
-            print("{}\n\n".format(text[i]), end="")
+        if text[i] in delimiters:
+            print(text[i] + "\n\n", end="")
             i += 1
-            while text[i] == " ":
+            while i < len(text) and text[i] == " ":
                 i += 1
         else:
-            print("{}".format(text[i]), end="")
+            print(text[i], end="")
             i += 1
