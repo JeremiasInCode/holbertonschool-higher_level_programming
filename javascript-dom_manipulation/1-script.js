b2415d811d67f07dbb25cade2fab7b2ed3b49d83
Jeremias Erba <jeremiaserba7894@gmail.com>
2024-01-30 17:27:34 -0300

    Task 1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,8 @@
+const header = document.querySelector("header");
+const div = document.querySelector("div");
+
+function changeColor() {
+  header.style.color = "red";
+}
+
+div.addEventListener('click', changeColor)
